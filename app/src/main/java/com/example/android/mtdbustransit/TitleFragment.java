52a67c8f2d528a6e0bf3413955486ced5c5f54a1
peterivanov22 <ivanov3@illinois.edu>
2015-07-15 02:22:40 -0500

    breh

---
+++ app/src/main/java/com/example/android/mtdbustransit/TitleFragment.java
@@ -0,0 +1,26 @@
+package com.example.android.mtdbustransit;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class TitleFragment extends Fragment {
+
+    public TitleFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ListView
+        textView.setOnClickListener(new View.OnClickListener());
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
+    }
+}
