6129f28071dc7470ccc14113875ec76b9a0de90e
peterivanov22 <ivanov3@illinois.edu>
2015-07-16 16:29:34 -0500

    brwe

--- app/src/main/java/com/example/android/mtdbustransit/TitleFragment.java
+++ app/src/main/java/com/example/android/mtdbustransit/TitleFragment.java
@@ -1,11 +1,13 @@
 package com.example.android.mtdbustransit;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.TextView;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -18,9 +20,42 @@ public class TitleFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        ListView
-        textView.setOnClickListener(new View.OnClickListener());
+
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        TextView favorites = (TextView) rootView.findViewById(R.id.favorites_textview);
+        TextView routeplanner = (TextView) rootView.findViewById(R.id.routeplanner_textview);
+        TextView searchStops = (TextView) rootView.findViewById(R.id.searchstops_textview);
+
+        favorites.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), RoutePlannerActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        routeplanner.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), RoutePlannerActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        searchStops.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+            }
+        });
+
+        TextView favorites_textview = (TextView) rootView.findViewById(R.id.favorites_textview);
+
         return rootView;
     }
 }
